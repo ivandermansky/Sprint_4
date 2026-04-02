4a2c0537bc439e430a4bcd1c78644065c8199ad3
ivandermansky <ivan.dermansky@gmail.com>
2026-04-02 10:41:47 +0300

    Создан и добавлен BaseTest
    Класс BaseTest, от которого будут наследоваться тесты OrderTest, QuestionsAndAnswersTest, UpperOrderButtonTest

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,29 @@
+package org.example.tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+
+
+ //Базовый класс для всех тестов — содержит общую логику запуска браузера и закрытия браузера
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @Before
+    public void setUp() {
+        // Инициализация драйвера
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
